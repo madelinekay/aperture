3d0451df81049271fdf71e8f6cfaf25d6d87da04
Madeline Undis <mundis@smith.edu>
2021-09-05 11:11:00 -0700

    Fix material-ui SSR style issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
@@ -7,6 +7,13 @@ import { UserContextProvider } from "../store/user-context";
 import { AuthContextProvider } from "../store/auth-context";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <AuthContextProvider>
       <UserContextProvider>
