887906e6d79372715657b34e633b2d8200fac60c
Madeline Undis <mundis@smith.edu>
2021-09-05 10:19:23 -0700

    Finish profile

--- components/MainNavigation.tsx
+++ components/MainNavigation.tsx
@@ -1,25 +1,86 @@
 import { FC, useContext } from "react";
 import Link from "next/link";
 import UserContext from "../store/user-context";
+import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import FaceIcon from "@material-ui/icons/Face";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PublicIcon from "@material-ui/icons/Public";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+
+const useStyles = makeStyles((theme) => ({
+  appbar: {
+    background: "white",
+    color: "black",
+  },
+  toolbar: {
+    marginRight: 240,
+    marginLeft: 240,
+  },
+  menu: {
+    display: "flex",
+  },
+  title: {
+    width: "15%",
+  },
+  navItem: {
+    marginRight: theme.spacing(1),
+  },
+  search: {
+    position: "relative",
+    marginLeft: 150,
+    width: "100%",
+  },
+  searchIcon: {
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    marginLeft: 30,
+  },
+}));
 
 const MainNavigation: FC = () => {
   const { username } = useContext(UserContext);
+  const classes = useStyles();
   return (
-    <header>
-      <div>Aperture</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/my-feed">Home</Link>
-          </li>
-          <li>
-            <Link href={"/" + username.toString()}>Profile</Link>
-          </li>
-          <li>Likes</li>
-          <li>World</li>
-        </ul>
-      </nav>
-    </header>
+    <AppBar position="sticky" className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <div className={classes.title}>Aperture</div>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <div className={classes.input}>
+            <InputBase placeholder="search..." />
+          </div>
+        </div>
+
+        <div className={classes.menu}>
+          <div className={classes.navItem}>
+            <Link href="/my-feed">
+              <HomeIcon />
+            </Link>
+          </div>
+          <div className={classes.navItem}>
+            <Link href={"/" + username.toString()}>
+              <FaceIcon />
+            </Link>
+          </div>
+          <div className={classes.navItem}>
+            <FavoriteIcon />
+          </div>
+          <div className={classes.navItem}>
+            <PublicIcon />
+          </div>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
--- pages/[username].tsx
+++ pages/[username].tsx
@@ -2,16 +2,65 @@ import { getFromFirebase, getStorageRef } from "../utils/firebase";
 import { NextPage } from "next";
 import UserContext from "../store/user-context";
 import { useContext, useEffect } from "react";
+import Image from "next/image";
+import { Hidden } from "@material-ui/core";
 
 const Home: NextPage = () => {
   const { images, username } = useContext(UserContext);
+  const padders = Array.from(Array(3 - (images.length % 3)));
 
   return (
-    <div>
-      <div>Profile</div>
-      <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: 1000,
+        margin: "0 auto",
+        // borderStyle: "solid",
+      }}
+    >
+      {/* <div>
+        <div>Madeline</div>
+        <div>0 followers</div>
+        <div>0 following</div>
+      </div> */}
+      <div
+        style={{
+          //   marginTop: '30px',
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          marginLeft: 30,
+          marginTop: 30,
+        }}
+      >
         {images.map((url) => (
-          <img src={url} key={url} />
+          <div
+            style={{
+              overflow: "hidden",
+              maxWidth: 290,
+              maxHeight: 290,
+              marginRight: 30,
+              marginBottom: 30,
+            }}
+          >
+            <img
+              src={url}
+              key={url}
+              style={{ maxHeight: 600, maxWidth: 600 }}
+            />
+          </div>
+        ))}
+        {padders.map((_, index) => (
+          <div
+            key={index}
+            css={`
+              maxWidth: 290,
+              maxHeight: 290,
+              overflow: "hidden",
+            
+            `}
+          />
         ))}
       </div>
     </div>
