0c1c3dd112c1e6eb42e83542bb0f52cc80a6c589
Madeline Undis <mundis@smith.edu>
2021-11-19 23:04:27 -0800

    added .env file

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -2,16 +2,15 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ONxO_Vjxt1HO8DKeoqcsV8ExTUsqof4",
-  authDomain: "aperture-479c6.firebaseapp.com",
-  databaseURL: "https://aperture-479c6-default-rtdb.firebaseio.com",
-  projectId: "aperture-479c6",
-  storageBucket: "aperture-479c6.appspot.com",
-  messagingSenderId: "407719756458",
-  appId: "1:407719756458:web:4421b56d4a0d2168fbc6d3",
-  measurementId: "G-JRQ46M1TP5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT,
 };
-
 export const getStorageRef = (userId: number) => {
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
